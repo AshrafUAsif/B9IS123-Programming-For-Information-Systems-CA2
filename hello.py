740fc0617ae91c674254cdb2a8f4ab19658e728e
Md. Ashraf Uddin <108550504+AshrafUAsif@users.noreply.github.com>
2024-04-06 17:54:33 +0000

    Initialized the basic flask app with a home page route '/' and rendering template.

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
-print("Hello")
+from flask import Flask, render_template
 
-print("Hey hi !! I am Manasaaaa")
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
